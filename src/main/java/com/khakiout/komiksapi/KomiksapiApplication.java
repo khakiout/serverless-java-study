deef690cf8189b7c39a833b9a102ee3c1dc1a8fc
Ruen Zapanta <rzapanta@stratpoint.com>
2019-07-20 21:58:56 +0800

    Setup initial project structure

---
+++ src/main/java/com/khakiout/komiksapi/KomiksapiApplication.java
@@ -0,0 +1,13 @@
+package com.khakiout.komiksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomiksapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KomiksapiApplication.class, args);
+	}
+
+}
