f068af6914cd34e82d17fd705d2f957a24bce0b2
Ruen Zapanta <rzapanta@stratpoint.com>
2019-08-20 17:31:14 +0800

    Add AuthenticationApplication service

---
+++ authentication/src/main/java/com/komiks/api/application/AuthenticationApplication.java
@@ -0,0 +1,63 @@
+package com.komiks.api.application;
+
+import com.komiks.api.domain.Session;
+import com.komiks.api.infrastructure.db.model.User;
+import com.komiks.api.infrastructure.db.repository.UserRepository;
+import com.komiks.api.model.request.LoginRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Handles the authentication process and generation of token.
+ */
+public class AuthenticationApplication {
+
+    private static final Logger logger = LogManager.getLogger(AuthenticationApplication.class);
+
+    private final UserRepository userRepository;
+
+    /**
+     * Initialize the authentication application.
+     *
+     * @param userRepository the user repository implementation.
+     */
+    public AuthenticationApplication(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        createUser("admin1", "admin");
+        createUser("admin2", "admin");
+        createUser("admin3", "admin");
+    }
+
+    /**
+     * Crude implementation of user login. In real life scenarios, the password should be
+     * salted/hashed.
+     *
+     * @param loginRequest the login request.
+     * @return the Session, if the loginRequest is valid.
+     */
+    public Session authenticate(LoginRequest loginRequest) {
+        User user = this.userRepository.getUser(loginRequest.username);
+        Session session = null;
+
+        if (user.getPassword().equals(loginRequest.password)) {
+            logger.info("User is valid.");
+            session = new Session();
+            session.setUsername(user.getUsername());
+            session.setToken("12312312312312321");
+        } else {
+            logger.info("User details is not valid.");
+        }
+
+        return session;
+    }
+
+    private void createUser(String username, String password) {
+        if (this.userRepository.getUser(username) == null) {
+            User user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+
+            this.userRepository.saveUser(user);
+        }
+    }
+}
--- authentication/src/main/java/com/komiks/api/interfaces/LoginHandler.java
+++ authentication/src/main/java/com/komiks/api/interfaces/LoginHandler.java
@@ -2,10 +2,13 @@ package com.komiks.api.interfaces;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.komiks.api.application.AuthenticationApplication;
 import com.komiks.api.commons.JsonUtils;
 import com.komiks.api.domain.Session;
+import com.komiks.api.infrastructure.db.repository.UserRepository;
+import com.komiks.api.infrastructure.db.repository.UserRepositoryImpl;
+import com.komiks.api.interfaces.http.models.response.GenericMessageResponse;
 import com.komiks.api.interfaces.http.models.response.GenericServerErrorResponse;
-import com.komiks.api.interfaces.http.models.response.NotFoundResponse;
 import com.komiks.api.model.request.LoginRequest;
 import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
@@ -19,25 +22,23 @@ public class LoginHandler implements RequestHandler<ApiGatewayRequest, ApiGatewa
     public ApiGatewayResponse handleRequest(ApiGatewayRequest request, Context context) {
 
         try {
-            // get the 'pathParameters' from input
+            UserRepository userRepository = new UserRepositoryImpl();
+            AuthenticationApplication application = new AuthenticationApplication(userRepository);
+
             LoginRequest loginRequest = JsonUtils.parseJson(request.getBody(), LoginRequest.class);
 
             logger.info("Logging in {}", loginRequest.username);
+            Session session = application.authenticate(loginRequest);
 
-            Session session = new Session();
-            session.setToken("1234");
-            session.setUsername(loginRequest.username);
-
-            // send the response back
-            if (loginRequest.password != null) {
+            if (session != null) {
                 return ApiGatewayResponse.builder()
                     .setStatusCode(200)
                     .setObjectBody(session)
                     .build();
             } else {
                 return ApiGatewayResponse.builder()
-                    .setStatusCode(404)
-                    .setObjectBody(new NotFoundResponse())
+                    .setStatusCode(401)
+                    .setObjectBody(new GenericMessageResponse("Credentials invalid"))
                     .build();
             }
         } catch (IOException ex) {
