c546bebbe11d490ac33b6d09e464c2f1d1987295
Ruen Zapanta <rzapanta@stratpoint.com>
2019-08-25 14:00:13 +0800

    Add sample node project

---
+++ hello-world/handler.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express')
+const hello = express()
+const sls = require('serverless-http')
+
+//Handle the GET endpoint on the root route /
+hello.get('/', async (req, res, next) => {
+ res.status(200).send('Hello everyone!')
+})
+hello.get('/world', async (req, res, next) => {
+ res.status(200).send('Hello World!!')
+})
+hello.get('/earth', async (req, res, next) => {
+ res.status(200).send('Hello Earth!!')
+})
+
+module.exports.server = sls(hello)
